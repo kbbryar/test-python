e6ef35caad31bb99fd60a0a71440a8bb288938b6
ken bryar <kbbryar@hotmail.com>
2021-11-23 19:22:24 -0600

    Added a pip installation into the setup.sh script

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ if [ ! -d ./venv ] ; then
     python3 -m venv venv
 fi
 
+source ./venv/bin/activate
+pip3 install -r ./requirements.txt
+deactivate
+
 echo "execute the following command 'source ./venv/bin/activate'"
